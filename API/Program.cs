492d5895fa01f5f63f735d8c8feae95251e65462
S.M.Owais <smowais.neduet@gmail.com>
2024-07-03 10:57:26 +0500

    setup project references

--- API/Program.cs
+++ API/Program.cs
@@ -1,37 +1,25 @@
-using API.Extensions;
-using API.Middlewares;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using Serilog;
-using Shared.Constants;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.ServicesRegistry(builder.Configuration);
-
-Log.Logger = new LoggerConfiguration()
-.MinimumLevel.Warning()
-    .WriteTo.File(MiscellaneousConstants._logpath, rollingInterval: RollingInterval.Day)
-    .CreateLogger();
-
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(MiscellaneousConstants._policy);
+
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
