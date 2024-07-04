9fc05915e6442b05c66b24d7cda1a1dcda53c60e
S.M.Owais <smowais.neduet@gmail.com>
2024-07-04 17:24:49 +0500

    architecture 107

--- Shared/Extensions/JSONResponse.cs
+++ Shared/Extensions/JSONResponse.cs
@@ -1,48 +1,29 @@
 ﻿using Shared.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
-namespace Shared.Extensions
+namespace Shared.Extensions;
+
+public static class JSONResponse
 {
-    public static class JSONResponse
+    public static async Task<ResponseModel> ToResponse(this Task task, bool status = true, int statusCode = 200, string message = "", object data = null)
     {
-        // Extension method for Task (asynchronous operations returning void)
-        public static async Task<ResponseModel> ToResponse(this Task task, bool status = true, int statusCode = 200, string message = "", object data = null)
+        await task;
+        return new ResponseModel
         {
-            try
-            {
-                await task; 
-                return new ResponseModel
-                {
-                    Status = status,
-                    StatusCode = statusCode,
-                    Message = message,
-                    Data = data
-                };
-            }
-            catch (CustomException ex)
-            {               
-                return new ResponseModel
-                {
-                    Status = false,
-                    StatusCode = (int)ex.StatusCode,
-                    Message = ex.Message
-                };
-            }
-        }
+            Status = status,
+            StatusCode = statusCode,
+            Message = message,
+            Data = data
+        };
     }
+}
 
 
-    public class ResponseModel
-    {
-        public bool Status { get; set; } = true;
-        public int StatusCode { get; set; } = 200;
-        public string Message { get; set; } = string.Empty;
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public object Data { get; set; }
-    }
+public class ResponseModel
+{
+    public bool Status { get; set; } = true;
+    public int StatusCode { get; set; } = 200;
+    public string Message { get; set; } = string.Empty;
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public object Data { get; set; }
 }
