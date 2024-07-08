2d76205a2bfbe465e09d592aed929c9b36b53281
S.M.Owais <smowais.neduet@gmail.com>
2024-07-08 11:05:38 +0500

    stripe integration

---
+++ Domain/Repositories/Services/IPaymentGateway.cs
@@ -0,0 +1,9 @@
+﻿using Stripe;
+
+namespace Domain.Repositories.Services;
+
+public interface IPaymentGateway
+{
+    Charge CreateCharge(string token, decimal amount, string currency = "usd");
+    Customer CreateCustomer(string email, string cardToken, string name = null);
+}
---
+++ Infrastructure/Services/PaymentGateway.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Repositories.Services;
+using Stripe;
+
+namespace Infrastructure.Services;
+
+public class PaymentGateway : IPaymentGateway
+{
+    public PaymentGateway()
+    {
+        StripeConfiguration.ApiKey = Appsettings.Instance.GetValue("Stripe:SecretKey");
+    }
+
+    public Charge CreateCharge(string token, decimal amount, string currency = "usd")
+    {
+        var options = new ChargeCreateOptions
+        {
+            Amount = (int)(amount * 100),
+            Currency = currency,
+            Source = token,
+            Description = "Test Charge"
+        };
+
+        return new ChargeService().Create(options);
+    }
+
+    public Customer CreateCustomer(string email, string cardToken, string name = null)
+    {
+        var options = new CustomerCreateOptions
+        {
+            Email = email,
+            Name = name,
+            Source = cardToken,
+        };
+
+        return new CustomerService().Create(options);
+    }
+}
