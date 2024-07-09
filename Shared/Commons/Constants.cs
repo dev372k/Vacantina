6fcee09f0a23ed87df454278e76b8ee313e34dce
S.M.Owais <smowais.neduet@gmail.com>
2024-07-09 17:27:46 +0500

    Update Constants.cs

--- Shared/Commons/Constants.cs
+++ Shared/Commons/Constants.cs
@@ -3,14 +3,12 @@
 public class MiscilenousConstants
 {
     public const string _policy = "CorsPolicy";
-    public const string IMAGEPATH = "Images";
-    public const string FILEPATH = "Files";
+    public const string IMAGEPATH = "files/images";
     public const string LOGPATH = "Logs/log.txt";
 }
 
 public class CacheKeys
 {
-    public const string TEST = "test";
     public const string USER = "users";
     public const string ROLE = "roles";
     public const string CATEGORY = "categories";
